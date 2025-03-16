4357c688d6fecde0c1ea557c1b8e05e507e49961
Gilary Rugeles Avendaño <gilaryv.rugelesa@autonoma.edu.co>
2025-03-16 13:56:02 -0500

    Finalizada ventana principla y Jdialog creados

---
+++ src/autonoma/BibliotecaPOO/main/Biblioteca.java
@@ -0,0 +1,19 @@
+
+package autonoma.BibliotecaPOO.main;
+
+import autonoma.BibliotecaPOO.views.VentanaPrincipal;
+
+/**
+ *
+ * @author jgiugtiñut
+ */
+public class Biblioteca {
+
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new  VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+        
+    }
+    
+}
